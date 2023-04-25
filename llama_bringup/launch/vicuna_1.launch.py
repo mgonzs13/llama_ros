e3d24fbceb49beabd7a3a2a415ace91748187b06
Miguel Ángel González Santamarta <mgons@unileon.es>
2023-04-25 23:30:15 +0200

    new vicuna v1.1

---
+++ llama_bringup/launch/vicuna_1.launch.py
@@ -0,0 +1,35 @@
+
+import os
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+    llama_bringup_shared_dir = get_package_share_directory(
+        "llama_bringup")
+
+    return LaunchDescription([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(llama_bringup_shared_dir, "launch", "base.launch.py")),
+            launch_arguments={
+                "n_threads": "4",
+                "n_predict": "512",
+                "repeat_last_n": "8",
+                "n_ctx": "512",
+                "n_batch": "8",
+                "top_k": "40",
+
+                "model": os.path.abspath(os.path.normpath(os.path.expanduser("~/llama_models/vicuna-1.1.bin"))),
+                "file": os.path.join(llama_bringup_shared_dir, "prompts/vicuna_1.txt"),
+
+                "prefix": "\n\nUSER:\n",
+                "suffix": "\n\nASSISTANT:\n",
+                "stop": "USER:\n",
+
+                "temp": "0.2"
+            }.items()
+        )
+    ])
