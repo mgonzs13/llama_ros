3c0b56cf6c4fa52749a2872c88850a6017d631a5
mgonzs13 <mgonzs13@users.noreply.github.com>
2024-11-21 11:55:26 +0000

    deploy: 679ac090676c999641dc73e0edeb1318ba628dbf

--- search/files_2.js
+++ search/files_2.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['chat_5fllama_5fros_2epy_0',['chat_llama_ros.py',['../chat__llama__ros_8py.html',1,'']]],
-  ['chat_5fprompt_5fformatter_2epy_1',['chat_prompt_formatter.py',['../chat__prompt__formatter_8py.html',1,'']]],
-  ['chatllama_5fdemo_5fnode_2epy_2',['chatllama_demo_node.py',['../chatllama__demo__node_8py.html',1,'']]]
+  ['chatllama_5fdemo_5fnode_2epy_1',['chatllama_demo_node.py',['../chatllama__demo__node_8py.html',1,'']]]
 ];
