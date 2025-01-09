148baa209e5e72111dc450af24534994fe92abc8
mgonzs13 <mgonzs13@users.noreply.github.com>
2025-01-09 22:22:47 +0000

    deploy: d309f5cff33ad0b49cba676669f2c023874d7e8e

--- search/all_17.js
+++ search/all_17.js
@@ -4,5 +4,6 @@ var searchData=
   ['value_5flength_1',['value_length',['../structllama__ros_1_1Metadata_1_1AttentionInfo.html#a38b92b211af8eefdfb85eb36729a20ff',1,'llama_ros::Metadata::AttentionInfo']]],
   ['vectorstore_2',['vectorstore',['../namespacellama__demos_1_1llama__rag__demo__node.html#a1e75aa82ea58d750b97bc8aea2263c50',1,'llama_demos::llama_rag_demo_node']]],
   ['version_3',['version',['../structllama__ros_1_1Metadata_1_1GeneralInfo.html#a74c0ccfa6a0f0347a47a53b563d5e367',1,'llama_ros::Metadata::GeneralInfo::version'],['../namespacesetup.html#a2aa722b36a933088812b50ea79b97a5c',1,'setup.version']]],
-  ['vlm_20demo_4',['VLM Demo',['../index.html#autotoc_md35',1,'']]]
+  ['vlm_4',['chat_llama_ros (Chat + VLM)',['../index.html#autotoc_md29',1,'']]],
+  ['vlm_20demo_5',['VLM Demo',['../index.html#autotoc_md35',1,'']]]
 ];
