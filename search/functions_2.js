8d44955ba2bf47027b8aa6f7dd8eaf5eaeadc869
mgonzs13 <mgonzs13@users.noreply.github.com>
2025-01-11 23:25:00 +0000

    deploy: 02d459b40ecd03f6176d8d9880681a48501a4664

--- search/functions_2.js
+++ search/functions_2.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['base64_5fencode_0',['base64_encode',['../classllava__ros_1_1LlavaNode.html#afbaaad12f3a156fb2306dc012d5434ac',1,'llava_ros::LlavaNode']]],
   ['base64_5fimage_5fto_5fembed_1',['base64_image_to_embed',['../classllava__ros_1_1Llava.html#ad5a902e782e913a8409d0f482ac46451',1,'llava_ros::Llava']]],
-  ['bind_5ftools_2',['bind_tools',['../classllama__ros_1_1langchain_1_1chat__llama__ros_1_1ChatLlamaROS.html#a69f356a9735498f6fe81b3c4388ad855',1,'llama_ros::langchain::chat_llama_ros::ChatLlamaROS']]]
+  ['bind_5ftools_2',['bind_tools',['../classllama__ros_1_1langchain_1_1chat__llama__ros_1_1ChatLlamaROS.html#a06d2b38a031c2f0fde1348d3474cdabe',1,'llama_ros::langchain::chat_llama_ros::ChatLlamaROS']]]
 ];
