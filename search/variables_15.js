bbb8be4541db4eaafdcb2e57ad4b0f379a9d3c31
mgonzs13 <mgonzs13@users.noreply.github.com>
2025-01-13 15:32:29 +0000

    deploy: 0171812946cfaf2bc0983fd5a75039e3a71c4cbc

--- search/variables_15.js
+++ search/variables_15.js
@@ -3,9 +3,9 @@ var searchData=
   ['unknown_5ftoken_5fid_0',['unknown_token_id',['../structllama__ros_1_1Metadata_1_1TokenizerInfo.html#af32aa6d0c2af0849c4970bfe523cc00a',1,'llama_ros::Metadata::TokenizerInfo']]],
   ['update_5floras_5fservice_5f_1',['update_loras_service_',['../classllama__ros_1_1LlamaNode.html#af66f3c472a2456c25852a1371afcac3e',1,'llama_ros::LlamaNode']]],
   ['url_2',['url',['../structllama__ros_1_1Metadata_1_1GeneralInfo.html#a39e51b97a1c2e97ed3dfcc54434b0d07',1,'llama_ros::Metadata::GeneralInfo']]],
-  ['use_5fgguf_5ftemplate_3',['use_gguf_template',['../classllama__ros_1_1langchain_1_1chat__llama__ros_1_1ChatLlamaROS.html#a314c773891e4727bbee93b372b18e2ef',1,'llama_ros::langchain::chat_llama_ros::ChatLlamaROS']]],
-  ['use_5fimage_4',['use_image',['../classllama__demos_1_1llava__demo__node_1_1LlavaDemoNode.html#ac4d9014a7b28bc3356f8d73079e9ad6b',1,'llama_demos::llava_demo_node::LlavaDemoNode']]],
-  ['use_5fllama_5ftemplate_5',['use_llama_template',['../classllama__ros_1_1langchain_1_1chat__llama__ros_1_1ChatLlamaROS.html#a87f5966e66091d9082ee2f70bf9fcf04',1,'llama_ros::langchain::chat_llama_ros::ChatLlamaROS']]],
+  ['use_5fdefault_5ftemplate_3',['use_default_template',['../classllama__ros_1_1langchain_1_1chat__llama__ros_1_1ChatLlamaROS.html#acf163d17fe19dc4b7d2a554dbb40e542',1,'llama_ros::langchain::chat_llama_ros::ChatLlamaROS']]],
+  ['use_5fgguf_5ftemplate_4',['use_gguf_template',['../classllama__ros_1_1langchain_1_1chat__llama__ros_1_1ChatLlamaROS.html#a314c773891e4727bbee93b372b18e2ef',1,'llama_ros::langchain::chat_llama_ros::ChatLlamaROS']]],
+  ['use_5fimage_5',['use_image',['../classllama__demos_1_1llava__demo__node_1_1LlavaDemoNode.html#ac4d9014a7b28bc3356f8d73079e9ad6b',1,'llama_demos::llava_demo_node::LlavaDemoNode']]],
   ['use_5fparallel_5fresidual_6',['use_parallel_residual',['../structllama__ros_1_1Metadata_1_1ModelInfo.html#a502b50f9c2b8a7fa60d69b51c17eed0e',1,'llama_ros::Metadata::ModelInfo']]],
   ['use_5fpenalty_5fprompt_5ftokens_7',['use_penalty_prompt_tokens',['../classllama__ros_1_1langchain_1_1llama__ros__common_1_1LlamaROSCommon.html#aa42948543e6dbb4f4068c0c08abda38d',1,'llama_ros::langchain::llama_ros_common::LlamaROSCommon']]],
   ['uuid_8',['uuid',['../structllama__ros_1_1Metadata_1_1GeneralInfo.html#a9dc4d8b30688d638b5e6b9efeb2730e1',1,'llama_ros::Metadata::GeneralInfo']]]
