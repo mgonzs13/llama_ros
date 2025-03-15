0573cc89d1d298b3709c5cd2f305b2203eed3112
Miguel Ángel González Santamarta <mgons@unileon.es>
2025-03-15 17:55:21 +0100

    temporal fix for structured demo

--- llama_demos/llama_demos/chatllama_structured_demo_node.py
+++ llama_demos/llama_demos/chatllama_structured_demo_node.py
@@ -85,13 +85,13 @@ class ChatLlamaStructuredDemoNode(Node):
         self.final_time = time.time()
 
         self.get_logger().info(f"Prompt: Tell me a joke about cats")
-        self.get_logger().info(f"Response: {response.content.strip()}")
+        self.get_logger().info(f"Response: {response}")
         self.get_logger().info(
             f"Time elapsed: {self.final_time - self.initial_time:.2f} seconds"
         )
-        self.get_logger().info(
-            f"Tokens per second: {response.usage_metadata['output_tokens'] / (self.final_time - self.initial_time):.2f} t/s"
-        )
+        # self.get_logger().info(
+        #     f"Tokens per second: {response.usage_metadata['output_tokens'] / (self.final_time - self.initial_time):.2f} t/s"
+        # )
 
 
 def main():
