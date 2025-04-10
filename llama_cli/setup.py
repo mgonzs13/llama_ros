14b28db09f387641dd5da6917acc2c3eb138bb36
Miguel Ángel González Santamarta <mgons@unileon.es>
2025-04-10 20:44:22 +0200

    new version 5.0.0

--- llama_cli/setup.py
+++ llama_cli/setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name="llama_cli",
-    version="4.5.0",
+    version="5.0.0",
     packages=find_packages(exclude=["test"]),
     zip_safe=True,
     author="Miguel Ángel González Santamarta",
