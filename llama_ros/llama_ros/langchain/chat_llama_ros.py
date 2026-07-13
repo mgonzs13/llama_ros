b96a758952e0f1c8fc68713218740aa59908b862
Miguel Ángel González Santamarta <mgons@unileon.es>
2026-07-13 13:37:06 +0200

    fix: remove unused import of is_basemodel_subclass from langchain_core.utils.pydantic

--- llama_ros/llama_ros/langchain/chat_llama_ros.py
+++ llama_ros/llama_ros/langchain/chat_llama_ros.py
@@ -59,7 +59,6 @@ from langchain_core.callbacks import (
 from langchain_core.messages.ai import (
     UsageMetadata,
 )
-from langchain_core.utils.pydantic import is_basemodel_subclass
 from langchain_core.utils.function_calling import convert_to_openai_tool
 from langchain_core.outputs import ChatGeneration, ChatResult, ChatGenerationChunk
 from langchain_core.runnables import Runnable, RunnableLambda
